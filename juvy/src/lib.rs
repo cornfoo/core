b58e4935a458816e9aa19859bf3b01d2da4c5011
JK <bowajk@gmail.com>
2025-12-19 13:04:06 +1100

    Add iOS build pipeline with UniFFI bindings

--- juvy/src/lib.rs
+++ juvy/src/lib.rs
@@ -1,14 +1,6 @@
+uniffi::setup_scaffolding!("juvy");
+
+#[uniffi::export]
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
